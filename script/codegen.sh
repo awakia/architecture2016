90b2a619948197f8bb3d49509cf77e7924198d0f
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-01-03 01:40:43 +0900

    Generate reverse_proxy code by grpc-gateway

--- script/codegen.sh
+++ script/codegen.sh
@@ -4,3 +4,7 @@ mkdir -p src/rpc
 
 # Generate service by gRPC
 protoc -Iproto -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis --go_out=Mgoogle/api/annotations.proto=github.com/gengo/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:src/rpc proto/*.proto
+
+# Generate reverse-proxy by grpc-gateway
+
+protoc -Iproto -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:src/rpc proto/*.proto
