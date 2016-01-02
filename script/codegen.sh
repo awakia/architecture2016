168622fc83e0dee5809c5ca7f2c22e034ab4b80a
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2016-01-03 01:39:46 +0900

    Change place to rpc

--- script/codegen.sh
+++ script/codegen.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mkdir -p src
-protoc -Iproto -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:src proto/*.proto
+mkdir -p src/rpc
+
+# Generate service by gRPC
+protoc -Iproto -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis --go_out=Mgoogle/api/annotations.proto=github.com/gengo/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:src/rpc proto/*.proto
